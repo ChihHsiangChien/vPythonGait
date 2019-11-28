bf560c335c5eaa4b757e0e072b985baa0d97380c
ChihHsiangChien <pancala@gmail.com>
2019-11-28 11:03:14 +0800

    add graph

--- gait.py
+++ gait.py
@@ -1,6 +1,6 @@
 from vpython import *
 
-# test
+
 """
 #Reading Data from serial
 import serial
@@ -32,9 +32,9 @@ scale = 0.1 #
 """
 產生坐標軸，做為參考用
 """
-xaxis = cylinder(pos=vector(0,0,0), axis=vector(10,0,0), color = vector(1,0,0), radius=0.05) #X axis
-yaxis = cylinder(pos=vector(0,0,0), axis=vector(0,10,0), color = vector(0,1,0), radius=0.05) #Y axis
-Zaxis = cylinder(pos=vector(0,0,0), axis=vector(0,0,10), color = vector(0,0,1), radius=0.05) #Z axis
+xaxis = cylinder(pos=vector(0,0,0), axis=vector(10,0,0), color = vector(1,0,0), radius=0.05, opacity = .5) #X axis
+yaxis = cylinder(pos=vector(0,0,0), axis=vector(0,10,0), color = vector(0,1,0), radius=0.05, opacity = .5) #Y axis
+Zaxis = cylinder(pos=vector(0,0,0), axis=vector(0,0,10), color = vector(0,0,1), radius=0.05, opacity = .5) #Z axis
 
                 
 """
@@ -49,10 +49,11 @@ hip = box(pos = vector(0,0,0),
           length = hip_l*scale,
           height = hip_h*scale,
           width = hip_w*scale,
-          color = vector(.3, .4, .5))
+          color = vector(.3, .4, .5),
+          opacity = .2)
 
 #髖關節位置
-hip_joint = vector(hip.pos.x + 1 ,0,0)
+hip_joint = sphere(pos=vector(hip.pos.x + 1 ,0,0),radius=0.5)
 
 
 #股骨setup
@@ -61,12 +62,14 @@ femur_length = 35
 femur_vertical_axis = vector(0,femur_length*scale*-1,0)
 
 #產生股骨
-femur = cylinder(pos = hip_joint,
+femur = cylinder(pos = hip_joint.pos,
                  axis = femur_vertical_axis,
                  color = vector(.5,.5,.5), radius= femur_radius * scale)
 #膝關節位置
 #print(femur.pos)
-knee = femur.pos + femur.axis
+knee_joint = sphere(pos= femur.pos + femur.axis,radius = 0.5,opacity = .9,
+                    make_trail=True,trail_color= color.green)
+
 
 #脛骨setup
 tibia_radius = 2
@@ -74,40 +77,82 @@ tibia_length = 35
 tibia_vertical_axis = vector(0,tibia_length*scale*-1,0)
 
 #產生脛骨
-tibia = cylinder(pos = knee,
+tibia = cylinder(pos = knee_joint.pos,
                  axis = tibia_vertical_axis,
                  color = vector(.5,.2,.2), radius= tibia_radius * scale)
 #踝關節位置
-ankle = tibia.pos + tibia.axis
+
+ankle_joint = sphere(pos= tibia.pos + tibia.axis ,radius = 0.4, opacity = .9,
+                     make_trail=True, trail_color= color.blue)
+                    #interval=10, retain=50
 
 #腳掌設定
-foot_radius = 8 #  腳掌寬度
+foot_radius = 3 #  腳掌寬度
 foot_length = 25 #  腳掌長度 
 dist_btw_ankle_midfoot = 7 #踝關節和腳中間的距離
 foot_verticla_axis = vector(0, foot_length * scale*-1, 0 )
 
 
 #產生腳掌
-foot = cylinder(pos = ankle,
+foot = cylinder(pos = ankle_joint.pos,
                 axis = foot_verticla_axis,
-                color = vector(.5,.2,.2), radius= foot_radius * scale)
+                color = vector(.2,.8,.2), radius= foot_radius * scale, opacity = .7)
 
 
 #攝影機角度
-scene.camera.pos = vector(10,0,5)
+scene.camera.pos = vector(20,0,0)
 scene.camera.axis = scene.camera.pos * -1
+
+
+#產生圖
+gr = graph(fast=False,
+           width=600, height=250,
+           title='<b>Leg Angle</b>',
+           xtitle='<i>time</i>', ytitle='<i>Angle</i>',
+           foreground=color.black, background=color.white,
+           xmin = 0, xmax=50, ymin=-45, ymax=180
+           
+           )
+hip_angle_curve = gcurve(color = color.red, label='hip')
+knee_angle_curve = gcurve(color = color.green,label='knee')
+ankle_angle_curve = gcurve(color = color.blue,label='ankle')
+
 
 i = 0 
 while True:
-    rate(1)
+    rate(5)
     
-    femur.axis = femur_vertical_axis.rotate(angle = radians( 20 + sin(i)*30 ), axis = proj(hip_joint,vector(1,0,0)) * -1)
-    knee = femur.pos + femur.axis
-    tibia.pos = knee
-    tibia.axis = tibia_vertical_axis.rotate(angle = radians( 40 + sin(i*.5 - 10)*10 ), axis = proj(knee,vector(1,0,0))) #tibia backward
-    ankle = tibia.pos + tibia.axis
+    hip_angle = 20 + sin(i)*30
+    knee_angle = 40 + sin(i*.5 - 10)*10  
+    ankle_angle = 90 + sin(i)*10
+    """
+    femur_angle = 0
+    tibia_angle = 90
+    foot_angle = 0
+    """                
+    femur.axis = femur_vertical_axis.rotate(angle = radians( hip_angle ), axis = vector(1,0,0) * -1)
+    #hip_arraw = arrow(pos=hip_joint.pos, axis= vector(5,0,0), shaftwidth=.3)
     
-    foot.pos = ankle + vector(0,0,dist_btw_ankle_midfoot*scale)
-    #foot.axis = foot_verticla_axis.rotate(angle = radians( sin(i)*30 ), axis = proj(ankle, vector(1,0,0) )*-1)
-    #foot.axis = foot_verticla_axis.rotate(angle = radians( sin(i)*30 ), axis = proj(ankle,vector(1,0,0)) *-1)
+    knee_joint.pos = femur.pos + femur.axis
+    
+    tibia.pos = knee_joint.pos
+    tibia.axis = tibia_vertical_axis.rotate(angle = radians(knee_angle  ), axis = vector(5,0,0) ) #tibia backward
+    #knee_arraw = arrow(pos=knee_joint.pos, axis= vector(5,0,0), shaftwidth=.1 , opacity = .2)
+                                           
+    ankle_joint.pos = tibia.pos + tibia.axis
+    foot.pos = ankle_joint.pos
+    foot.axis = foot_verticla_axis.rotate(angle = radians( ankle_angle ), axis = vector(1,0,0)*-1)
+    #ankle_arraw = arrow(pos=ankle_joint.pos, axis= vector(5,0,0), shaftwidth=.1 ,opacity = .3)
+    
+    
+
+    if i > gr.xmax:
+        d = i-gr.xmax
+        gr.xmin += d
+        gr.xmax += d
+        
+    hip_angle_curve.plot( i, hip_angle )
+    knee_angle_curve.plot( i, knee_angle )
+    ankle_angle_curve.plot( i, ankle_angle )
+    
     i += 1
